71937024816cb6eaa8e55d5a566fe385a1b03a5b
Victor Santana <134336920+VitalHugal@users.noreply.github.com>
2025-05-07 22:23:55 -0300

    up

--- blue_sky/app/ProcessVideo.php
+++ blue_sky/app/ProcessVideo.php
@@ -165,50 +165,24 @@ class ProcessVideo
                 // $cmd320 = "$ffmpegPath -y -noautorotate -i \"$withFramePath\" -i \"$moldura320Path\" -filter_complex \"[0:v]scale=320:480,crop=320:448:0:0[scaled];[scaled][1:v]overlay=0:0\" -b:v 9000k -minrate 9000k -maxrate 10000k -x264-params nal-hrd=cbr -bufsize 20000k -fs 20M -preset ultrafast \"$destinationPath320/$fileName\"";
                 // $cmd1080 = "$ffmpegPath -y -noautorotate -i \"$withFramePath\" -i \"$molduraPath\" -filter_complex \"[0:v]scale=1080:1920:[scaled];[scaled][1:v]overlay=0:0\" -b:v 9000k -minrate 9000k -maxrate 10000k -x264-params nal-hrd=cbr -bufsize 20000k -fs 20M -preset ultrafast \"$destinationPath1080/$fileName\"";
 
-                // $exit = "[1:v]format=rgba[moldura_alpha]; " .
-                //     "[bg][moldura_alpha]overlay=0:0[out]\" " .
-                //     "-map \"[out]\" -t 10 -r 30 -an -c:v libx264 " .
-                //     "-preset fast ";
-
-                $cmd1080 = "$ffmpegPath -y " .
-                    "-i \"$withFramePath\" " .               // input 0: vídeo de fundo
-                    "-i \"$molduraPath\" " .                 // input 1: moldura com alpha
-                    "-filter_complex \"" .
-                    "[0:v]scale=1080:1920[bg]; " .
-                    "[1:v]format=rgba[moldura_alpha]; " .
+                $exit = "[1:v]format=rgba[moldura_alpha]; " .
                     "[bg][moldura_alpha]overlay=0:0[out]\" " .
                     "-map \"[out]\" -t 10 -r 30 -an -c:v libx264 " .
-                    // "-b:v 9000k -minrate 9000k -maxrate 10000k " .
-                    // "-x264-params nal-hrd=cbr -bufsize 20000k -fs 20M " .
-                    // "-maxrate 10000k " .
-                    // "-bufsize 20000k " .
-                    "-preset fast " .
-                    // "-preset veryslow " .
-                    "\"$destinationPath1080/$fileName\"";
-
-                $cmd320 = "$ffmpegPath -y " .
-                    "-i \"$withFramePath\" " .
-                    "-i \"$moldura320Path\" " .
-                    "-filter_complex \"" .
-                    "[0:v]scale=320:480,crop=320:448:0:16[bg]; " .
-                    "[1:v]format=rgba[moldura_alpha]; " .
-                    "[bg][moldura_alpha]overlay=0:0[out]\" " .
-                    "-map \"[out]\" -t 10 -r 30 -an " .
-                    "-c:v libx264 " .
-                    // "-b:v 9000k -minrate 9000k -maxrate 10000k " .
-                    // "-x264-params nal-hrd=cbr -bufsize 20000k -fs 20M " .
-                    // "-maxrate 10000k " .
-                    // "-bufsize 20000k " .
-                    "-preset fast " .
-                    // "-preset veryslow " .
-                    "\"$destinationPath320/$fileName\"";
+                    "-preset fast ";
 
                 // $cmd1080 = "$ffmpegPath -y " .
-                //     "-i \"$withFramePath\" " .
-                //     "-i \"$molduraPath\" " .
+                //     "-i \"$withFramePath\" " .               // input 0: vídeo de fundo
+                //     "-i \"$molduraPath\" " .                 // input 1: moldura com alpha
                 //     "-filter_complex \"" .
                 //     "[0:v]scale=1080:1920[bg]; " .
-                //     "$exit " .
+                //     "[1:v]format=rgba[moldura_alpha]; " .
+                //     "[bg][moldura_alpha]overlay=0:0[out]\" " .
+                //     "-map \"[out]\" -t 10 -r 30 -an -c:v libx264 " .
+                //     // "-b:v 9000k -minrate 9000k -maxrate 10000k " .
+                //     // "-x264-params nal-hrd=cbr -bufsize 20000k -fs 20M " .
+                //     // "-maxrate 10000k " .
+                //     // "-bufsize 20000k " .
+                //     "-preset fast " .
                 //     // "-preset veryslow " .
                 //     "\"$destinationPath1080/$fileName\"";
 
@@ -217,12 +191,38 @@ class ProcessVideo
                 //     "-i \"$moldura320Path\" " .
                 //     "-filter_complex \"" .
                 //     "[0:v]scale=320:480,crop=320:448:0:16[bg]; " .
-                //     "$exit " .
+                //     "[1:v]format=rgba[moldura_alpha]; " .
+                //     "[bg][moldura_alpha]overlay=0:0[out]\" " .
+                //     "-map \"[out]\" -t 10 -r 30 -an " .
+                //     "-c:v libx264 " .
+                //     // "-b:v 9000k -minrate 9000k -maxrate 10000k " .
+                //     // "-x264-params nal-hrd=cbr -bufsize 20000k -fs 20M " .
+                //     // "-maxrate 10000k " .
+                //     // "-bufsize 20000k " .
+                //     "-preset fast " .
                 //     // "-preset veryslow " .
                 //     "\"$destinationPath320/$fileName\"";
 
-                shell_exec($cmd1080);
-                shell_exec($cmd320);
+                $cmd1080 = "$ffmpegPath -y " .
+                    "-i \"$withFramePath\" " .
+                    "-i \"$molduraPath\" " .
+                    "-filter_complex \"" .
+                    "[0:v]scale=1080:1920[bg]; " .
+                    "$exit " .
+                    // "-preset veryslow " .
+                    "\"$destinationPath1080/$fileName\"";
+
+                $cmd320 = "$ffmpegPath -y " .
+                    "-i \"$withFramePath\" " .
+                    "-i \"$moldura320Path\" " .
+                    "-filter_complex \"" .
+                    "[0:v]scale=320:480,crop=320:448:0:16[bg]; " .
+                    "$exit " .
+                    // "-preset veryslow " .
+                    "\"$destinationPath320/$fileName\"";
+
+                shell_exec($cmd1080 && $cmd320);
+                // shell_exec($cmd320);
 
                 if (!file_exists($destinationPath1080 . DIRECTORY_SEPARATOR . $fileName) || !file_exists($destinationPath320 . DIRECTORY_SEPARATOR . $fileName)) {
 
